c95d43c791a0220f9f038293337882096f52de1a
Ayesha Atique <119811504+Ayeshaatique1234@users.noreply.github.com>
2022-12-05 01:24:04 +0500

    Add files via upload

---
+++ NoGender.java
@@ -0,0 +1,90 @@
+package automationexample;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class NoGender {
+//
+	@Test
+		public void test() throws InterruptedException {
+		//ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://demoqa.com/automation-practice-form");
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.id("firstName")).sendKeys("Ayesha");
+		
+		driver.findElement(By.id("lastName")).sendKeys("Atique");
+		
+		driver.findElement(By.id("userEmail")).sendKeys("aiqueayesha6@gmail.com");
+
+	    driver.findElement(By.id("userNumber")).sendKeys("1234561231");
+	    
+	    driver.findElement(By.id("subjectsInput")).sendKeys("Mathematic");
+	    
+	    driver.findElement(By.xpath("//*[@id=\"dateOfBirthInput\"]")).click();
+	    new WebDriverWait (driver, Duration.ofSeconds(5)).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("react-datepicker__month")));
+	    String monthYearVal = driver.findElement(By.className("react-datepicker__current-month")).getText();
+	    System.out.println(monthYearVal);
+	    String month = monthYearVal.split(" ")[0].trim();
+	    String year = monthYearVal.split(" ")[1].trim();
+	    while(!(month.equals("June") && year.equals("2021"))) {
+		   driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/button[1]")).click();
+		    monthYearVal = driver.findElement(By.className("react-datepicker__current-month")).getText();
+		   System.out.println(monthYearVal);
+		    month = monthYearVal.split(" ")[0].trim();
+		    year = monthYearVal.split(" ")[1].trim();
+	   }
+	    driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div[4]")).click();
+        
+        driver.findElement(By.id("subjectsInput")).sendKeys("Mathematic");
+        driver.findElement(By.id("subjectsInput")).sendKeys(Keys.TAB);
+		
+
+		 
+	        WebElement element = driver.findElement(By.xpath("//*[@id=\"hobbies-checkbox-1\"]"));
+	        Actions actions = new Actions(driver);
+	        //driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	        actions.moveToElement(element).click();
+        
+      
+        WebElement UploadImg = driver.findElement(By.id("uploadPicture"));
+        UploadImg.sendKeys("C:\\Users\\Ayesha-DPA\\Desktop\\app.png");
+        
+        driver.findElement(By.id("currentAddress")).sendKeys("House, Street, City, Country");
+        driver.findElement(By.id("react-select-3-input")).sendKeys("Lahore");
+        driver.findElement(By.id("react-select-3-input")).sendKeys(Keys.TAB);
+        driver.findElement(By.id("react-select-4-input")).sendKeys("Pakistan");
+        driver.findElement(By.id("react-select-4-input")).sendKeys(Keys.TAB);
+        
+        driver.findElement(By.id("submit")).sendKeys(Keys.ENTER);
+        
+       
+
+        String color = driver.findElement(By.xpath("//*[@id=\"genterWrapper\"]/div[2]/div[1]/label")).getCssValue("Color");
+        String[] numbers = color.replace("rgb(", "").replace(")", "").split(",");
+        int r = Integer.parseInt(numbers[0].trim());
+        int g = Integer.parseInt(numbers[1].trim());
+        int b = Integer.parseInt(numbers[2].trim());
+        System.out.println("r: " + r + "g: " + g + "b: " + b);
+        String hex = "#" + Integer.toHexString(r) + Integer.toHexString(g) + Integer.toHexString(b);
+        System.out.println(hex);
+        
+        Assert.assertTrue(hex.equals("#dc3545"));
+
+		driver.close();
+		
+	}
+
+}
